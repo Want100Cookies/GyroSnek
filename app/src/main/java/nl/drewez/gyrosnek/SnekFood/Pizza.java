50f0d0e0028313b5b27c169ed227487949111ba8
Pascal Drewes <pascal@drewes-webdesign.nl>
2017-03-27 17:03:07 +0200

    Add pizza

--- app/src/main/java/nl/drewez/gyrosnek/SnekFood/Pizza.java
+++ app/src/main/java/nl/drewez/gyrosnek/SnekFood/Pizza.java
@@ -3,21 +3,22 @@ package nl.drewez.gyrosnek.SnekFood;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
+
 import nl.drewez.gyrosnek.R;
 
-public class HotDog implements ISnekFood {
+public class Pizza implements ISnekFood {
     private int score = 1;
     private int y;
     private int x;
 
-    public HotDog(int x, int y) {
+    public Pizza(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
     @Override
     public Drawable getDrawable(Context context) {
-        return ContextCompat.getDrawable(context, R.drawable.pizza);
+        return ContextCompat.getDrawable(context, R.drawable.hog_dog);
     }
 
     @Override
