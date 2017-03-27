375266d95ce2e9a509caca83d864409fa3f116cd
Pascal Drewes <pascal@drewes-webdesign.nl>
2017-03-27 14:44:50 +0200

    Add CheeseBurger

--- app/src/main/java/nl/drewez/gyrosnek/SnekFood/ISnekFood.java
+++ app/src/main/java/nl/drewez/gyrosnek/SnekFood/ISnekFood.java
@@ -1,9 +1,10 @@
 package nl.drewez.gyrosnek.SnekFood;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 
 public interface ISnekFood {
-    Drawable getDrawable();
+    Drawable getDrawable(Context context);
     int getScore();
     int getX();
     int getY();
